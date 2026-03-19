13ac4b1041a932205dae0f6f67ae29ff03e7d5f9
sofia <100496081@alumnos.uc3m.es>
2026-03-19 14:33:33 +0100

    MainActivity.kt comments

--- app/src/main/java/com/example/pearpressure/MainActivity.kt
+++ app/src/main/java/com/example/pearpressure/MainActivity.kt
@@ -14,16 +14,17 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+    //MainViewModel: objeto donde se guardan y gestionan datos como asignaturas y exámenes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // hace que la interfaz pueda ocupar más pantalla, llegando hasta los bordes
 
         lifecycleScope.launch {
             viewModel.error.collect { err ->
                 err?.let { Log.e("Firebase", "Error: $it") }
             }
-        }
+        } //Esto escucha errores del ViewModel y los manda al log.
 
         setContent {
             SofiaTestTheme {
