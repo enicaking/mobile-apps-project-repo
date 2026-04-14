ed558db0d90bcf1a914e458b496a4c133707bbef
sofia <100496081@alumnos.uc3m.es>
2026-04-14 18:44:25 +0200

    FCM configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")  // Firebase
+    implementation("com.google.firebase:firebase-messaging-ktx") //FCM
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -71,4 +72,5 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.firebase.messaging) //FCM
 }
