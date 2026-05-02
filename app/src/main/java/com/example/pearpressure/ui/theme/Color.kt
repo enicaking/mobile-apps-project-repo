9003a3c4fd6bd9a2296f3347566548d56b5519e4
Enica King <kingenica@gmail.com>
2026-05-02 16:33:21 +0200

    moving statuspill colors to Colors.kt

--- app/src/main/java/com/example/pearpressure/ui/theme/Color.kt
+++ app/src/main/java/com/example/pearpressure/ui/theme/Color.kt
@@ -17,3 +17,17 @@ val OnBackground = Color(0xFF1C1B1F)
 
 val Surface = Color(0xFFFFFBFF)
 val OnSurface = Color(0xFF1C1B1F)
+
+
+// Status pill colors
+val StatusInProgressBg  = Color(0xFFE3F2FD)
+val StatusInProgressFg  = Color(0xFF1565C0)
+
+val StatusWaitingExpBg  = Color(0xFFFFF3E0)
+val StatusWaitingExpFg  = Color(0xFFEF6C00)
+
+val StatusWaitingFinBg  = Color(0xFFF3E5F5)
+val StatusWaitingFinFg  = Color(0xFF7B1FA2)
+
+val StatusFinishedBg    = Color(0xFFE8F5E9)
+val StatusFinishedFg    = Color(0xFF2E7D32)
